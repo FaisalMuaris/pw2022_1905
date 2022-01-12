fac0353ce410775c2e0202a373323431a19da073
Faisal Muaris <faisal.muaris.fm@gmail.com>
2022-01-12 13:47:41 +0700

    Menambahkan fungsi hapus ubah dan pencarian

---
+++ Kuliah/pertemuan 3/detail.php
@@ -0,0 +1,35 @@
+<?php
+require 'functions.php';
+
+//ambil id dari url
+$id = $_GET['id'];
+
+// Query Mahasiswa berdasarkan id 
+$mhs = query("SELECT * FROM mahasiswa WHERE id = $id");
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Detail Mahasiswa</title>
+</head>
+
+<body>
+  <h3>Detail Mahasiswa</h3>
+
+  <ul>
+    <li><img src="img/<?= $mhs['gambar']; ?>" width="120"></li>
+    <li>NRP : <?= $mhs['nrp']; ?></li>
+    <li>Nama : <?= $mhs['nama']; ?></li>
+    <li>Email : <?= $mhs['email']; ?></li>
+    <li>Jurusan : <?= $mhs['jurusan']; ?></li>
+    <li><a href="ubah.php?id=<?= $mhs['id']; ?>">Ubah</a> | <a href="hapus.php?id=<?= $mhs['id']; ?>" onclick="return confirm('Apakah anda yakin menghapus data?');">Hapus</a></li>
+    <li><a href="index.php">Kembali Ke halaman Mahasiswa</a></li>
+  </ul>
+</body>
+
+</html>
